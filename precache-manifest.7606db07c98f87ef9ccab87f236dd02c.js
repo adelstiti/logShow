11b573a2a3020ccda63231836362115743310e0f
Adel Stiti <adelstiti@users.noreply.github.com>
2020-01-14 23:26:18 +0100

    Updates

---
+++ precache-manifest.7606db07c98f87ef9ccab87f236dd02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8972d86c0bac1a3b84bf03ae39234bed",
+    "url": "/logShow/index.html"
+  },
+  {
+    "revision": "35309c91fa8cd6a5fb28",
+    "url": "/logShow/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "a5f3580841966a1d2946",
+    "url": "/logShow/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "35309c91fa8cd6a5fb28",
+    "url": "/logShow/static/js/2.4b5ebaba.chunk.js"
+  },
+  {
+    "revision": "a5f3580841966a1d2946",
+    "url": "/logShow/static/js/main.82cad6c0.chunk.js"
+  },
+  {
+    "revision": "53074bbb4ab42da3f7f1",
+    "url": "/logShow/static/js/runtime-main.781c28f5.js"
+  }
+]);
